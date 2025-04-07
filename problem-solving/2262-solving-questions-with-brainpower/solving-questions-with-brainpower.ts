781f564b597963af1f73744ef447d74c11b690cc
amykhsub <amykhdev+ghsub@gmail.com>
2025-04-07 12:45:08 -0500

    Time: 22 ms (50.59%) | Memory: 94.5 MB (28.61%) - LeetSync

--- problem-solving/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.ts
+++ problem-solving/2262-solving-questions-with-brainpower/solving-questions-with-brainpower.ts
@@ -1,40 +1,30 @@
 /**
- * Approach: Top-down Dynamic Programming with Memoization, Recursion, Depth-First Search.
+ * Approach: Bottom-Up Dynamic Programming, Iterative.
  * 
- * `dp` array stores the maximum points achievable from each question index onwards.
- *     This is a memoization technique to avoid redundant calculations.
+ * The loop iterates backward from the second-to-last question to the 1st question.
+ * Iteration initialized with points for current question, calculates the index of the next question.
+ * If the index in scope of questions, add those points to current question weight,
+ *    and make sure with Math.max(...) that each i+1 question includes weight of follow questions.
  * 
- * `dfs` helper calculates the maximum points starting from a given question index `i`
- *     and returns the maximum points achievable.
- * 
- * `if (i >= questions.length)` - base case of recursion. `true` means we reached the end of question list.
- * `if (dp[i]) return dp[i]` - memoization step.
- * 
- * `dp[i] = Math.max(...)` is core of Dynamic Programming approach. `Math.max(...)` represents the best choice.
- * - The 1st argument Math.max(<solve>, ...) represents the case to solve the question.
- *       We add current question points and recursively call `dfs` skipping the required number of questions.
- * - The 2nd argument Math.max(..., <skip>) represents the case to skip the current question `i`,
- *       recursively call `dfs` for the next question.
- * 
- * Time: O(n). The `dfs` function is called at most once for each index `i` due to memoization.
- * Space: O(n). It requires O(n) space to store he results for each subproblem.
+ * Time:  O(n). Questions iteration.
+ * Space: O(n). It requires O(n) space to store the maximum points for each subproblem.
  */
-let dp: number[];
-
-const dfs = (questions: number[][], i: number): number => {
-    if (i >= questions.length) return 0;
-    if (dp[i]) return dp[i];
-    
-    dp[i] = Math.max(
-        questions[i][0] + dfs(questions, i + questions[i][1] + 1),
-        dfs(questions, i + 1)
-    )
-    
-    return dp[i];
-};
-
 function mostPoints(questions: number[][]): number {
-    dp = Array<number>(questions.length);
+    const n = questions.length;
+    const dp = Array<number>(n);
+    let i = n - 1;
     
-    return dfs(questions, 0);
+    dp[i] = questions[i][0];
+
+    for (--i; i >= 0; i--) {
+        dp[i] = questions[i][0];
+        
+        const nextId = i + questions[i][1] + 1;
+        
+        if (nextId < n) dp[i] += dp[nextId];
+
+        dp[i] = Math.max(dp[i], dp[i+1]);
+    }
+
+    return dp[0];
 };
