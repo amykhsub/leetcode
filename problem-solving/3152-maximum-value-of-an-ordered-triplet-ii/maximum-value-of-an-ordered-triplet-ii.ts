c042fc7c91c591a964b591a28c35fe97cdb16e95
amykhsub <amykhdev+ghsub@gmail.com>
2025-04-07 17:28:40 -0500

    Time: 2 ms (92.33%) | Memory: 68.2 MB (77.11%) - LeetSync

---
+++ problem-solving/3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.ts
@@ -0,0 +1,35 @@
+/**
+ * Approach: Greedy with single traversal
+ * 
+ * The algorithm uses the idea of iteratively updating a maximum value as it traverses the input.
+ * It avoids nested loops by maintaining maxK and maxDiff.
+ * While the idea is similar to Kadane's algorithm 
+ * (which is is specifically designed to find the maximum sum of a contiguous subarray within a one-dimensional array),
+ * this solution is not finding a contiguous subarray,
+ * because it's based on a specific formula that utilizes the maximum values.
+ * 
+ * The loop iterates through the nums array from left to right.
+ * The calculations are performed in the backward order:
+ * 1) maxV is 3rd part of the triplet calculation.
+ * 2) maxDiff is 2nd part of the triplet calculation.
+ * 3) maxK is 1st part of the triplet calculation.
+ * Because we want to use the values from previous iteration before they are updated.
+ * This approach helps traverse the input with single loop
+ * and avoid additional calculation with "i" inside each iteration.
+ * 
+ * Time: O(n)
+ * Space: O(1)
+ */
+function maximumTripletValue(nums: number[]): number {
+    let maxV = 0;
+    let maxDiff = 0;
+    let maxK = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        maxV = Math.max(maxV, maxDiff * nums[i]);
+        maxDiff = Math.max(maxDiff, maxK - nums[i]);
+        maxK = Math.max(maxK, nums[i]);
+    }
+
+    return maxV;
+};
